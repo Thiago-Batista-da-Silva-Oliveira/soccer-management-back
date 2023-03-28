689ff179b89ec8aa992bbb7ea558a700c57b225c
Thiago <gmoliveira69@gmail.com>
2023-03-27 21:54:34 -0300

    get transactions routes

--- src/modules/transactions/infra/routes/transactionsRoutes.ts
+++ src/modules/transactions/infra/routes/transactionsRoutes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 import { validateTransaction } from "../../middlewares";
 import { AddTransactionController } from "../../useCases";
+import { GetTransactionsController } from "../../useCases/GetTransactions";
 
 export const transactionsRoutes = Router();
 
 const addTransactionController = new AddTransactionController();
+const getTransactionsController = new GetTransactionsController();
 
 transactionsRoutes.post(
   "/make-transaction",
   validateTransaction,
   addTransactionController.handle
 );
+transactionsRoutes.get(
+  "/transactions",
+  getTransactionsController.handle
+);
+
